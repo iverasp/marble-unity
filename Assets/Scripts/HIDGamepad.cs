a6e8a9a7f315a8a571a52c9d8c31907fbbeec4f7
Iver Egge <iver.egge@knowit.no>
2019-05-29 19:12:22 +0200

    Mesh colliders not working properly

--- Assets/Scripts/HIDGamepad.cs
+++ Assets/Scripts/HIDGamepad.cs
@@ -15,17 +15,6 @@ public class HIDGamepad : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // requires you to set up axes "Joy0X" - "Joy3X" and "Joy0Y" - "Joy3Y" in the Input Manger
-        /*
-        for (int i = 0; i < 4; i++)
-        {
-            if (Mathf.Abs(Input.GetAxis("Joy" + i + "X")) > 0.2 ||
-                Mathf.Abs(Input.GetAxis("Joy" + i + "Y")) > 0.2)
-            {
-                Debug.Log(Input.GetJoystickNames()[i] + " is moved");
-            }
-        }
-        */
         boardWrapper.transform.rotation = Quaternion.Euler(Input.GetAxis("JoyY") * 9 * -1, 0, Input.GetAxis("JoyX") * 9 * -1);
         Debug.Log("X: " + Input.GetAxis("JoyX") + ", JoyY: " + Input.GetAxis("JoyY"));
     }
